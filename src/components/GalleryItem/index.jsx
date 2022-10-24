bd6b334e06a3384997cb81ec0fe270321d514879
MrFacundo <ftroitero@gmail.com>
2022-10-24 22:47:23 +0100

    added sections translations

--- src/components/GalleryItem/index.jsx
+++ src/components/GalleryItem/index.jsx
@@ -1,48 +1,49 @@
 import React, { useEffect, useRef } from "react";
 import useOnScreen from "../../hooks/useOnScreen";
 import cn from "classnames";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+
 import "./style.scss";
 
-
 export default function GalleryItem({
-    img_src,
-    excerpt,
-    subtitle,
-    title,
-    updateActiveItem,
-    index,
-  }) {
-    const ref = useRef(null);
-    const onScreen = useOnScreen(ref, 0.5);
-  
-    useEffect(() => {
-      if (onScreen) {
-        updateActiveItem(index);
-      }
-    }, [onScreen, index, updateActiveItem]);
-  
-    return (
-      <div
-        className={cn("gallery-item-wrapper", { "is-reveal": onScreen })}
-        ref={ref}
-      >
-        <div></div>
-        <div className={"gallery-item"}>
-          <div className="gallery-item__info">
-            <div className="gallery-info__heading">
-              <h1 className="gallery-info__title">{title}</h1>
-              <h2 className="gallery-info__subtitle">{subtitle}</h2>
-            </div>
-            <p className="gallery-info__excerpt">
-              {excerpt}
-            </p>
+  img_src,
+  excerpt,
+  subtitle,
+  title,
+  updateActiveItem,
+  index,
+}) {
+  const ref = useRef(null);
+  const onScreen = useOnScreen(ref, 0.5);
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    if (onScreen) {
+      updateActiveItem(index);
+    }
+  }, [onScreen, index, updateActiveItem]);
+
+  return (
+    <div
+      className={cn("gallery-item-wrapper", { "is-reveal": onScreen })}
+      ref={ref}
+    >
+      <div></div>
+      <div className={"gallery-item"}>
+        <div className="gallery-item__info">
+          <div className="gallery-info__heading">
+            <h1 className="gallery-info__title">{t(title)}</h1>
+            <h2 className="gallery-info__subtitle">{t(subtitle)}</h2>
           </div>
-          <div
-            className="gallery-item__image"
-            style={{ backgroundImage: `url(${img_src})`}}
-          ></div>
+          <p className="gallery-info__excerpt">{t(excerpt)}</p>
         </div>
-        <div></div>
+        <div
+          className="gallery-item__image"
+          style={{ backgroundImage: `url(${img_src})` }}
+        ></div>
       </div>
-    );
-  }
+      <div></div>
+    </div>
+  );
+}
--- src/data.js
+++ src/data.js
@@ -1,4 +1,3 @@
-
 import image1 from "./images/atletas.jpg";
 import image2 from "./images/general.jpg";
 import image3 from "./images/limpiezas.jpg";
@@ -6,12 +5,13 @@ import image4 from "./images/registros.jpg";
 import image5 from "./images/metodo.jpg";
 import image6 from "./images/atletas_mobile.jpg";
 
+
 const gallery_items = [
   {
-    title: "Método de Sanación Asistida",
+    title: "gallery_item_1_title",
     subtitle: "",
     excerpt:
-      "Creado a partir de diferentes técnicas que he ido aprendiendo a lo largo de mi trayectoria, este método consiste en trabajar en el inconsciente para sanar creencias limitantes (como por ejemplo el no sentirse merecedor, no sentirse valioso, creer que uno no puede), así como resolver conflictos con el dinero, de relaciones laborales, personales, de pareja. También trata dolores físicos, traumas, adicciones, estados de depresión, ansiedad, entre otros. Buscamos convertir las creencias instauradas en creencias potenciadoras que nos ayuden a vivir una vida feliz y en paz.",
+      "gallery_item_1_text",
     excerpt_mobile:
       "...es un método creado a partir de diferentes técnicas que he ido aprendiendo a lo largo de mi trayectoria. Consiste en trabajar desde inconsciente para sanar creencias limitantes y convertirlas en creencias potenciadoras que nos ayuden a vivir una vida feliz y en paz.",
 
@@ -19,10 +19,10 @@ const gallery_items = [
     img_src_mobile: "",
   },
   {
-    title: "Sanación",
-    subtitle: "para Atletas de Alto Rendimiento",
+    title: "gallery_item_2_title",
+    subtitle: "gallery_item_2_subtitle",
     excerpt:
-      "Todos los atletas tienen una meta u objetivo. A través de este método, mi labor consiste en trabajar la concentración, focalizar en la templanza, instruir la mente para obtener mayor tranquilidad en los momentos más críticos.",
+      "gallery_item_2_text",
     excerpt_mobile:
       "A través del Método de Sanación Asistida buscamos trabajar la concentración, focalizarnos en la templanza e instruir la mente para obtener mayor tranquilidad en los momentos más críticos.",
     img_src: image1,
@@ -30,30 +30,30 @@ const gallery_items = [
 
   },
   {
-    title: "Sanación Asistida",
-    subtitle: "General",
+    title: "gallery_item_3_title",
+    subtitle: "gallery_item_3_subtitle",
     excerpt:
-      "Tratamos de encontrar el conflicto para sanarlo en el inconsciente, aplicando la canalización, don que descubrí hace algunos años y cada vez fue creciendo más y más. Muchas veces nos cuesta hablar o expresarnos y este método nos permite ver más allá e ir a la raíz del conflicto para poder sanar. En el caso de las dolencias físicas, el procedimiento es el mismo: luego de encontrar la raíz que ocasiona el dolor o la sintomatología, sbuscamos sanarla.",
+      "gallery_item_3_text",
     excerpt_mobile:
       "Aquí nos enfocamos en encontrar el conflicto para sanarlo en el inconsciente aplicando la canalización, don que descubrí hace algunos años.  Cuando encontramos la raíz que ocasiona el dolor o sintomatología, se sana.",
     img_src: image2,
     img_src_mobile: "",
   },
   {
-    title: "Limpezas Energéticas",
-    subtitle: "y Péndulo Hebreo",
+    title: "gallery_item_4_title",
+    subtitle: "gallery_item_4_subtitle",
     excerpt:
-      " ¿Como darnos cuenta si necesitamos una limpieza energética? Muchas veces en la vida empezamos a sentir que las cosas se estancan, que no fluyen como nos gustarían, que nos vinculamos con gente que no es positiva y atraemos situaciones o personas que no nos agradan. Esto puede significar la necesidad de una limpieza energética, que realizada desde el amor y la experiencia, nos haga volver a sentirnos nosotros mismos, bien, y que logre hacer que todo fluya nuevamente.",
+      "gallery_item_4_text",
     excerpt_mobile:
       "Muchas veces nos ocurre que empezamos a sentir que las cosas se estancan, que no fluyen como nos gustarían. Es aquí cuando necesitamos una limpieza energética para volver a sentirnos nosotros mismos.",
     img_src: image3,
     img_src_mobile: "",
   },
   {
-    title: "Registros",
-    subtitle: "Akáshicos",
+    title: "gallery_item_5_title",
+    subtitle: "gallery_item_5_subtitle",
     excerpt:
-      "Los Registros Akáshicos son una memoria universal de la existencia. Representan un espacio multidimensional dónde se “guardan” todas las experiencias del alma, que incluyen todos los conocimientos y experiencias de las vidas pasadas, la vida presente y las potencialidades futuras. A través de una lectura de Registros Akáshicos podemos comprender la realidad de una manera mucho más profunda y sanadora.",
+      "gallery_item_5_text",
     excerpt_mobile:
       "Los Registros Akáshicos son una memoria universal de la existencia. Representan un espacio donde se “guardan” todas las experiencias y conocimientos del alma. A través de su lectura podemos comprender la realidad de una manera profunda y sanadora.",
     img_src: image4,
